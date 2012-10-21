a539ac85f1854dcace50814c00272027012e925a
azeemyousaf <azeem.yousaf@gmail.com>
2012-10-21 13:40:35 +0500

    hw-2-n-3

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,60 @@
+class MoviesController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    #@movies = Movie.all
+    @movies = Movie.order(sort_column + " " + sort_direction).filter(filter_selection)
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = (params[:ratings].present? ? params[:ratings] : [])
+    session[:p] = params
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path (session[:p])
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path (session[:p])
+  end
+  
+  private
+  
+  def filter_selection
+    params[:ratings] == nil ? Movie.all_ratings : params[:ratings]
+  end
+  
+  def sort_column
+    Movie.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+end
